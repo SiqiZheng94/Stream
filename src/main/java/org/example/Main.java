06a54d90dd4228d206dd88c3c91475be69ee6ac3
siqizheng <cn.siqizheng@gmail.com>
2023-11-16 00:19:50 +0100

    v.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,91 @@
 package org.example;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+
+        List<Integer> evenNumbers = numbers.stream()
+                                        .filter(number->number%2==0)
+                                        .collect(Collectors.toList());
+        System.out.println(evenNumbers);
+
+        List<Integer> doubleNumbers = numbers.stream()
+                                        .map(number->number*2)
+                                        .collect(Collectors.toList());
+        System.out.println(doubleNumbers);
+
+        List<Integer> sortedNumbers = numbers.stream()
+                                      .sorted()
+                                      .collect(Collectors.toList());
+        System.out.println(sortedNumbers);
+
+        int sum = numbers.stream()
+                .reduce(0,(x,y)->x+y);
+        System.out.println(sum);
+
+        numbers.stream()
+                //.forEach(number->System.out.println(number));
+                .forEach(System.out::println);
+
+        //List<Integer> newList = numbers.stream().collect(Collectors.toList());
+        List<Integer> newList = new ArrayList<>(numbers);
+        System.out.println(newList);
+
+
+
+
+        //Copy a file "students.csv" to the root directory of this project.
+        try{
+            //Stream<String> lines = Files.lines(Path.of("students.csv"));
+            List<String> allLines = Files.readAllLines(Paths.get("/Users/siqizheng/IdeaProjects/Stream/src/main/java/org/example/students.csv"));
+            allLines.forEach(System.out::println);
+
+            Stream<String> dataLines = allLines.stream().skip(1);
+            List<Student> students = dataLines
+                    .map(line-> {
+                        String[] parts = line.split(",");
+                        if (parts.length != 4) {
+                            return null;
+                        }
+                        try {
+                            int id = Integer.parseInt(parts[0].trim());
+                            String name = parts[1].trim();
+                            String postalCode = parts[2].trim();
+                            int age = Integer.parseInt(parts[3].trim());
+                            return new Student(id, name, postalCode, age);
+                        } catch (NumberFormatException e) {
+                            return null;
+                        }
+                    })
+                    .filter(student -> student!=null)
+                    .distinct()
+                    .collect(Collectors.toList());
+
+            students.forEach(System.out::println);
+        } catch (IOException e){
+            e.printStackTrace();
         }
+
+
+
+
     }
+
 }
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,2 +1,25 @@
-package org.example;public class Student {
+package org.example;
+
+public class Student {
+    private int id;
+    private String name;
+    private String postalCode;
+    private int age;
+
+    public Student(int id, String name, String postalCode, int age) {
+        this.id = id;
+        this.name = name;
+        this.postalCode = postalCode;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", postalCode='" + postalCode + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
